556b60240e30709bd57f4ebe104fb4eaf3d14356
garugan <196047075+garugan@users.noreply.github.com>
2025-03-12 13:34:17 +0000

    [Update]devise_model

--- db/migrate/20250312133136_devise_create_users.rb
+++ db/migrate/20250312133136_devise_create_users.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-class DeviseCreateUses < ActiveRecord::Migration[6.1]
+class DeviseCreateUsers < ActiveRecord::Migration[6.1]
   def change
-    create_table :uses do |t|
+    create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -35,13 +35,12 @@ class DeviseCreateUses < ActiveRecord::Migration[6.1]
       ## 名前を保存するカラム
       t.string :name
 
-
       t.timestamps null: false
     end
 
-    add_index :uses, :email,                unique: true
-    add_index :uses, :reset_password_token, unique: true
-    # add_index :uses, :confirmation_token,   unique: true
-    # add_index :uses, :unlock_token,         unique: true
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
+    # add_index :users, :confirmation_token,   unique: true
+    # add_index :users, :unlock_token,         unique: true
   end
 end
