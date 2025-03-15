b20004b6ca34af74bf7ea1df9a89fd2f153de195
garugan <196047075+garugan@users.noreply.github.com>
2025-03-15 23:03:17 +0000

    [Add]user_edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
-  def edit
-  end
-
   def show
     @user = User.find(params[:id])
-    
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
 end
