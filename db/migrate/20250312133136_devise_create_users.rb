dc59c6f2a13e02d978169ee1495196a7b013d717
garugan <196047075+garugan@users.noreply.github.com>
2025-03-17 14:24:15 +0000

    [Update]header,1n

--- db/migrate/20250312133136_devise_create_users.rb
+++ db/migrate/20250312133136_devise_create_users.rb
@@ -32,9 +32,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      ## 名前を保存するカラム
       t.string :name
-      ## 自己紹介を保存するカラム
       t.text :introduction
 
       t.timestamps null: false
