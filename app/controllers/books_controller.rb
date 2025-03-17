072b92d6b5d0626e19436024e9bcea4856aea518
garugan <196047075+garugan@users.noreply.github.com>
2025-03-17 20:04:37 +0000

    [Add]validation,flash

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,8 +2,13 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    book.user = current_user
+    if book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(book.id)
+    else
+      redirect_to books_path
+    end
   end
 
   def index
@@ -14,6 +19,7 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @user = User.first
   end
 
   def edit
@@ -22,8 +28,12 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
